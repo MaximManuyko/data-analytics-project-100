18d8c4097abb325128111013e357aaf71a7344d6
MaximManuyko <128226331+MaximManuyko@users.noreply.github.com>
2023-12-25 03:55:19 +0300

    Add

--- hexlet-code/node_modules/ra-core/dist/cjs/form/useFormValues.d.ts
+++
@@ -1,5 +0,0 @@
-import { FieldValues } from 'react-hook-form';
-export declare const useFormValues: <TFieldValues extends FieldValues = FieldValues>() => {
-    [x: string]: any;
-} & TFieldValues;
-//# sourceMappingURL=useFormValues.d.ts.map
